c3ecbfcc1153e86f6bb4920c7d55cf1cc9f5c5f5
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2022-01-31 23:12:07 +0800

    Add Toggle Map
    Add buttons to toggle maps.
    Installed React Bootstrap

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,14 +1,69 @@
 import React from "react";
+// import test from "/img/test.svg";
+// import lobbyImg from "../../../public/img/Lobby.png";
+import { Button } from "react-bootstrap";
 
 function Map() {
+  let birdEyeViewMap = document.getElementById("bird-eye-view-map");
+  let mapName = document.getElementById("map-name");
+
+  function changeMap(location) {
+    birdEyeViewMap.src = require(`../../assets/${location}.png`);
+    mapName.innerText = location;
+  }
   return (
-    <div style={{ border: "5px outset red" }}>
-      <img
+    <div
+      style={{
+        border: "5px outset red",
+        // backgroundImage: `url(require("../../assets/Lobby.png"))`,
+      }}
+    >
+      <div>
+        <div id="map-name">Lobby</div>
+        <img
+          id="bird-eye-view-map"
+          src={require("../../assets/Lobby.png")}
+          alt="Tembusu Lobby"
+          width="50%"
+        />
+      </div>
+
+      <Button
+        onClick={() => {
+          changeMap("Lobby");
+        }}
+      >
+        Lobby
+      </Button>
+      <Button
+        onClick={() => {
+          changeMap("Lvl3");
+        }}
+      >
+        Level 3
+      </Button>
+      <Button
+        onClick={() => {
+          changeMap("Lvl17");
+        }}
+      >
+        Level 17
+      </Button>
+      <Button
+        onClick={() => {
+          changeMap("Learnlobe");
+        }}
+      >
+        Learn Lobe
+      </Button>
+
+      {/* <img
         id="map"
-        src={require("../../img/Lobby.png")}
-        alt="Tembusu Lobby"
+        src={process.env.PUBLIC_URL + "/img/Lobby_clickable.svg"}
+        alt="Tembusu Lobby Clickable"
         width="50%"
-      />
+        style={{ position: "absolute", left: 0, width: "100%" }}
+      /> */}
     </div>
   );
 }
