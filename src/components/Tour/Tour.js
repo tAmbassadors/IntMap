5e4a167ef1ec52bb43f143c5616203b54705ffbd
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2022-02-16 02:33:44 +0800

    clean up Tour/Svg.js code

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -12,21 +12,12 @@ export default function Tour(props) {
         <p1>Tour</p1>
         <p>{location}</p>
       </div>
-      <div class="img-container">
+      <div id="tour-div" class="img-container tour-div">
         <img
           id="tour-img"
           src={`${process.env.PUBLIC_URL}/assets/tour-img/${location}.jpg`}
           alt="Tembusu Lobby"
         />
-        <object id="tour-svg" className="map-overlay" type="image/svg+xml">
-          placeholder image
-        </object>
-        {/* <Button variant="primary" className="right-button">
-          Next
-        </Button>
-        <Button variant="primary" className="left-button" onClick>
-          Previous
-        </Button> */}
         <Svg
           id="map-overlay"
           location={location}
