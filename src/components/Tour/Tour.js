16dbaee6cf47d2913f32e8932923b760f80a48f4
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-02-19 15:06:55 +0800

    Change styling to use columns

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,55 +1,53 @@
-import React, { useEffect } from "react";
-import { Button, FormControl } from "react-bootstrap";
+import React, {useEffect} from "react";
+import {
+    Button,
+    Container,
+    FormControl,
+    Row,
+    Col
+} from "react-bootstrap";
 import "./Tour.css";
 import Svg from "./Svg.js";
-import { roomNames, roomDescriptions } from "./Description.js";
+import {roomNames, roomDescriptions} from "./Description.js";
 
 export default function Tour(props) {
-  const { location, changeLocation } = props;
-  console.log(location);
+    const {location, changeLocation} = props;
+    console.log(location);
 
-  useEffect(() => {
-    let tourDiv = document.getElementById("tour-div");
-    let tourName = document.getElementById("location-name");
-    let tourDesc = document.getElementById("location-description");
-    tourDiv.style.backgroundImage = `url(${process.env.PUBLIC_URL}/assets/tour-img/${location}.JPG)`;
-    if (roomNames[location]) {
-      tourName.innerText = roomNames[location];
-    } else {
-      tourName.innerText = "Tembusu College";
-    }
-    if (roomDescriptions[location]) {
-      tourDesc.innerText = roomDescriptions[location];
-    } else {
-      tourDesc.innerText = "Welcome to Tembusu College!";
-    }
-  }, [location]);
+    useEffect(() => {
+        let tourDiv = document.getElementById("tour-div");
+        let tourName = document.getElementById("location-name");
+        let tourDesc = document.getElementById("location-description");
+        tourDiv.style.backgroundImage = `url(${
+            process.env.PUBLIC_URL
+        }/assets/tour-img/${location}.JPG)`;
+        if (roomNames[location]) {
+            tourName.innerText = roomNames[location];
+        } else {
+            tourName.innerText = "Tembusu College";
+        }
+        if (roomDescriptions[location]) {
+            tourDesc.innerText = roomDescriptions[location];
+        } else {
+            tourDesc.innerText = "Welcome to Tembusu College!";
+        }
+    }, [location]);
 
-  return (
-    <div>
-      <div
-      // className="justiry-content-center"
-      >
-        <div
-          id="tour-div"
-          className="img-container tour-div"
-          // class="col-sm-8"
-        >
-          <Svg
-            id="map-overlay"
-            location={location}
-            changeLocation={changeLocation}
-          />
-        </div>
-        <div>
-          <p id="location-name">lobby</p>
-          <p id="location-description">
-            This is the main lobby of the Residential Building, where Tembusians
-            often meet up before heading off for meals, lessons or other
-            activities!
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col id="tour-div" className="img-container tour-div">
+                    <Svg id="map-overlay"
+                        location={location}
+                        changeLocation={changeLocation}/>
+                </Col>
+                <Col>
+                    <p id="location-name">lobby</p>
+                    <p id="location-description">
+                        This is the main lobby of the Residential Building, where Tembusians often meet up before heading off for meals, lessons or other activities!
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
