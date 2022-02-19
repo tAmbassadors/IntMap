b44969a19e56e7d0f3121b7d593aede6f323840d
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-02-19 15:06:30 +0800

    Add footer to website

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from "react";
+import {
+    Button,
+    Container,
+    FormControl,
+    Row,
+    Col
+} from "react-bootstrap";
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <Container>
+            <h1>Footer</h1>
+        </Container>
+    );
+}
