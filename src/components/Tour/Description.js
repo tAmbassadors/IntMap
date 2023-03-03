b5a45de3d9f9b9962ab84036248eab4f609e80c1
Edmund <e0406924@u.nus.edu>
2023-03-03 15:52:23 +0800

    updated description for OH'2023

--- src/components/Tour/Description.js
+++ src/components/Tour/Description.js
@@ -3,7 +3,7 @@ export const roomNames = {
   lobby: "Lobby",
   "sem-room": "Seminar Room",
   "reading-room": "Reading Room",
-  abbey: "Abbey",
+  abbey: "The Abbey",
   "box-office": "Box Office",
   oasis: "Oasis",
   "blank-canvas": "Blank Canvas",
@@ -25,25 +25,25 @@ export const roomNames = {
   "room-1": "Room",
   pantry: "Pantry",
   laundry: "Laundry Room",
-  "start-image": "Explore Tembusu",
+  "start-image": "Explore Tembusu College",
 };
 
 export const roomDescriptions = {
-  mph: "A dynamic space, the Multi-Purpose Hall is utilised by many Tembusu Interest Groups, from sports to performing arts groups. Be it Badminton, Rollerblading or Dance, Tembusu is home to a wide array of IGs for you to discover fun activities you’d enjoy! This hall also serves as the place where our College hosts its flagship event, Tembusu Forums. ",
+  mph: "A dynamic space, the Multi-Purpose Hall is utilised by many Tembusu College’s Interest Groups, from sports to performing arts. Be it Badminton, Rollerblading or Dance, the College is home to a wide array of IGs for you to discover fun activities you’d enjoy! This hall also serves as the place where we hosts our flagship event, the Tembusu Conversations.",
   lobby:
     "This is the main lobby of the Residential Building, where Tembusians often meet up before heading off for meals, lessons or other activities!",
   "sem-room":
-    "Seminar Rooms are a key feature of the Learn Lobe - they form the environment in which learning takes flight! One can look forward to both Junior Seminars (JS) and Senior Seminars (SS) as well as Ideas and Exposition Modules (IEMs) being held in these classrooms. Many Tembusians can vouch for the riveting modules they have attended under the University Town College Programme. \n \n In addition to the interesting topics that the modules at Tembusu explore, Seminar Rooms offer an intimate learning environment where you’ll be able to have lively class discussions with your peers. Furthermore, if you’re looking for a conducive space to get some work done or to run student activities, our Seminar Rooms can be booked for such purposes as well!",
+    "Seminar Rooms are a key feature of the Learn Lobe - they form the environment in which learning takes flight! One can look forward to both Junior Seminars (JS) and Senior Seminars (SS), as well as Ideas and Exposition Modules (IEMs), being held in these classrooms. Many Tembusians can vouch for the riveting courses they have attended under the University Town College Programme. \n \n In addition to the interesting topics that the courses at Tembusu College explore, Seminar Rooms offer an intimate learning environment where you’ll be able to have lively class discussions with your peers. Furthermore, if you’re looking for a conducive space to get some work done or to run student activities, our Seminar Rooms can be booked for such purposes as well!",
   "reading-room":
-    "Curated by our Tembusu Rector and Fellows, the Reading Room is home to a collection of books for students to discover and expand their reading repertoire. It is also the dynamic space where Tembusu College holds its Student’s Teas. ",
+    "Curated by our Tembusu College Rector and Fellows, the Reading Room is home to a collection of books for students to discover and expand their reading repertoire. It is also the dynamic space where Tembusu College holds its Student’s Teas. ",
   abbey:
-    "If you love music, you’re at the right place! The Abbey is Tembusu’s very own music studio. Equipped with state-of-the-art sound systems and instruments, the stage is set for you and like-minded musicians to jam and have fun!",
+    "If you love music, you’re at the right place! The Abbey is Tembusu College’s very own music studio. Equipped with state-of-the-art sound systems and instruments, the stage is set for you and like-minded musicians to jam and have fun!",
   "box-office":
-    "Whether you are a coffee lover, film buff, or simply looking for a space to chill, The Box Office has got you covered! Not only is it Tembusu’s own home cinema, it is also a place where you can play games, relax  and bond over making coffee. ",
+    "Whether you are a coffee lover, film buff, or simply looking for a space to chill, The Box Office has got you covered! Not only is it Tembusu College’s own home cinema, it is also a place where you can play games, relax  and bond over making coffee. ",
   oasis:
     "The Oasis is a safe space for you to take a breather from the daily stresses of life. With mental health and emotional wellbeing at the heart of The Oasis, take the opportunity to retreat and recharge, be it alone or alongside peers and dear friends. ",
   "blank-canvas":
-    "The Blank Canvas is home to some of our Arts interest groups. It is a blank canvas to hone your creative expression. Be it painting, craft making and the like, this space beckons you to explore!  ",
+    "The Blank Canvas is home to some of our Arts Interest Groups. It is a blank canvas to hone your creative expression. Be it painting, craft making and the like, this space beckons you to explore!  ",
   launchpad:
     "Apart from being an exciting space for ideation, expression, education and everything in between, the Launchpad serves as a mini-studio for Tembusians to conduct photoshoots and creative works. The sky’s the limit!",
   entrance:
@@ -59,24 +59,24 @@ export const roomDescriptions = {
   "learnlobe-walkway-B1-2":
     "This is Level B1 of the Learn Lobe, where you will find The Abbey, The Blank Canvas, Reading Room, Launchpad and yet another Seminar Room!",
   "drop-off-point":
-    "Our college is situated in University Town and is accessible by car and public transport. If you’re coming by car, this is the drop-off point you’ll see! It is conveniently located between the Multi-Purpose Hall and the side entrance to the main college building. You can also find your way to University Town by bus from the various MRT stations nearby, such as Kent Ridge, Clementi and Buona Vista.",
+    "Tembusu College is situated in University Town and is accessible by car and public transport. If you’re coming by car, this is the drop-off point you’ll see! It is conveniently located between the Multi-Purpose Hall and the side entrance to the main college building. You can also find your way to University Town by bus from the various MRT stations nearby, such as Kent Ridge, Clementi and Buona Vista.",
   "dining-hall":
     "Like clockwork, the Dining Hall is bustling with Tembusians ready to start their day with a lovely breakfast or end it off with a hearty dinner with peers. Our dining hall offers different dining options (Noodles, Asian, Western, Indian and Muslim) for a variety of dietary preferences!",
   "common-lounge":
     "The Common Lounge is a space used by our students for a myriad of purposes. Social gatherings, discussions, study sessions, Student's Teas and House events or dinners are just some of the many activities that take place here. Furnished with a piano and pool table, the Common Lounge is definitely the place to be for those looking to bond over some fun activities!",
   "misty-garden":
-    "A lovely garden located at Level 3, the Misty Garden (named after the inaugural Tembusu Master’s cat) represents Tembusu’s commitment to biodiversity, the natural environment and sustainability. Tembusu Urban Farmers is one student group reflective of such an ethos - and they’re the team which helps tend to this garden!",
+    "A lovely garden located at Level 3, Misty Garden (named after the inaugural Tembusu College Master’s cat), represents the College’s commitment to biodiversity, the natural environment and sustainability. Tembusu College Urban Farmers is one student group reflective of such an ethos - and they’re the team that helps tend to this garden!",
   mcl: "The Master’s Common Lounge is a cozy room located on the 3rd floor of Tembusu College. Master's Teas are hosted by the College Master in this space, whereby both local and international guests are invited to share their work, views, and life experiences with students. These sessions open the possibility for students to meet, mingle and expand one’s perspective through an informal, small-group setting.",
   "level-lounge-1":
-    "Tired after a long day but not ready to head to bed just yet? No worries! Each level has a Level Lounge where you can study or unwind with your friendly neighbours. Here’s a fun fact: Our houses are named after endangered Asian animal species, to represent the College’s appreciation and concern for biodiversity and conservation. The 5 houses are: Shan (Snow Leopard), Ora (Komodo Dragon), Gaja (Asian Elephant), Tancho (Red-Crowned Crane) and Ponya (Red Panda).",
+    "Tired after a long day but not ready to head to bed just yet? No worries! Each level has a Level Lounge where you can study or unwind with your friendly neighbours. Here’s a fun fact: Our Houses are named after endangered Asian animal species, to represent the College’s appreciation and concern for biodiversity and conservation. The 5 Houses are: Shan (Snow Leopard), Ora (Komodo Dragon), Gaja (Asian Elephant), Tancho (Red-Crowned Crane) and Ponya (Red Panda).",
   corridor:
     "Along the corridors of our residential building, you can often feel the breeze or admire the loveliest sunsets across UTown! There are rooms along both sides of the corridor –  navigate to see what our rooms and suites look like!",
   suite:
-    "In Tembusu, you can either opt for a room in a suite or one along the corridor. Both allow you to choose between an air-conditioned or non-air-conditioned room. Suite living entails a shared living space with a table and sofas in addition to your personal room! There is also a bathroom (consisting of 2 shower stalls and 2 toilet cubicles) to share with your 5 other suitemates.",
+    "In Tembusu College, you can either opt for a room in a suite or one along the corridor. Both allow you to choose between an air-conditioned or non-air-conditioned room. Suite living entails a shared living space with a table and sofas, in addition to your personal room! There is also a bathroom (consisting of 2 shower stalls and 2 toilet cubicles) to be shared with your 5 other suitemates.",
   "room-1":
     "While each room is furnished with the same desk, bed, shelf and wardrobe, every Tembusian decorates their room differently, making this space special and uniquely their own. ",
   pantry:
-    "Every house in the College has their own pantry. You can conveniently pop in to prepare light meals, air-fry a snack or refrigerate your own food and drink items. ",
+    "Every House in the College has their own pantry. You can conveniently pop in to prepare light meals, air-fry a snack or refrigerate your own food and drink items. ",
   laundry:
     "Located on Levels 9 and 17, the laundry rooms are well-equipped with both washing machines and dryers to ensure that every Tembusian has access to crisp and clean clothes! ",
   "start-image":
